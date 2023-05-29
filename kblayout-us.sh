e7c8197fdbcb99fea17f4e054230906680d1229b
Yuto Nishida <yuto@berkeley.edu>
2023-05-29 00:20:38 -0700

    Add US kblayout switcher

---
+++ kblayout-us.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Raycast Script Command Template
+#
+# Duplicate this file and remove ".template." from the filename to get started.
+# See full documentation here: https://github.com/raycast/script-commands
+#
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Select Keyboard Layout as US using macism
+# @raycast.mode fullOutput
+# @raycast.packageName macism-ext
+#
+# Optional parameters:
+# @raycast.icon ⌨️
+# @raycast.currentDirectoryPath ~
+# @raycast.needsConfirmation false
+#
+# Documentation:
+# @raycast.description Uses macism to switch to the US keyboard layout
+# @raycast.author Yuto Nishida
+# @raycast.authorURL https://yut.to/github
+
+macism com.apple.keylayout.US
