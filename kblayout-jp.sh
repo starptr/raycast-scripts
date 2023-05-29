31ea2c320c4ec9fad0edec5ed5c70367caec4a6d
Yuto Nishida <yuto@berkeley.edu>
2023-05-29 00:29:55 -0700

    Add de, jp, unicode

---
+++ kblayout-jp.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Raycast Script Command Template
+#
+# Duplicate this file and remove ".template." from the filename to get started.
+# See full documentation here: https://github.com/raycast/script-commands
+#
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Select JP Keyboard Layout using macism
+# @raycast.mode silent
+# @raycast.packageName macism-ext
+#
+# Optional parameters:
+# @raycast.icon ⌨️
+# @raycast.currentDirectoryPath ~
+# @raycast.needsConfirmation false
+#
+# Documentation:
+# @raycast.description Uses macism to switch to the US keyboard layout
+# @raycast.author Yuto Nishida
+# @raycast.authorURL https://yut.to/github
+
+macism com.apple.inputmethod.Kotoeri.RomajiTyping.Japanese
